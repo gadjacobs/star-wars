f4700c98c3336fac1d3720ef5a0300cd79e88b13
Gad Jacobs <gadexofficial@gmail.com>
2019-08-13 03:49:28 -0700

    Font and typography

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
       </nav>
       <div class="tc ph3">
 
-        <h1 class="f2 f1-l fw2 white-90 mb0 lh-title"><img src={process.env.PUBLIC_URL + "/assets/logo.png"} alt='' width="130px" height="auto" /> Directory</h1>
+        <h1 class="f2 f1-l white-90 mb0 lh-title"><img src={process.env.PUBLIC_URL + "/assets/logo.png"} alt='' width="130px" height="auto" /> Directory</h1>
         <hr width="25%" />
         <h2 class="fw1 f3 white-80">Find your favorite Characters, Films, Species,</h2> <h2 class="fw1 f3 white-80">Starships and Planets</h2>
         <input
